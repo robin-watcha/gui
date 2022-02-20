786cf8b3f6f39f9e1e7cb4580363c39e7e2fd12c
robin <robin@watcha.com>
2022-02-20 22:38:23 +0900

    제네릭과 트레이트 경계를 이용해 구현

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+extern crate gui;
+use gui::Draw;
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+    }
+}
+
+use gui::Screen;
+
 fn main() {
-    println!("Hello, world!");
+    let screen = Screen {
+        components: vec![
+            (SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            (SelectBox {
+                width: 50,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+        ],
+    };
+
+    screen.run();
 }
